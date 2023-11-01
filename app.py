818e0ceada6559f087cb26cd6da30d04bca6b95c
Jimmy Wirth <jimmybean98@gmail.com>
2023-11-01 09:40:53 -0400

    PythonPlayground

---
+++ app.py
@@ -0,0 +1,10 @@
+# birth_year = input("Enter your birth year: ")
+# age = 2023 - float(birth_year)
+# print(age)
+
+num1string = input("Input a number to add: ")
+num1float = float(num1string)
+num2string= input("Input a number to add: ")
+num2float = float(num2string)
+sum=num1float + num2float
+print("The sum is: "+str(sum))
